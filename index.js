1ab033e3e32f6aa6a61d6845f84a248cccc91e72
koho20002000@yahoo.com Berryman <koho20002000@yahoo.com>
2018-07-07 04:01:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout (string){
+  return string.toUpperCase()
+}
+function whisper (string){
+  return string.toLowerCase()
+}
+function logShout (string){
+  console.log (string.toUpperCase());
+}
+function logWhisper (string){
+  console.log (string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if (string.toLowerCase()===string){
+  retun "I can't hear You"
+  }
+  
+  if (string.toUpperCase ()===string){
+    return "YES INDEED!"
+  }
+  
+  if (string=== 'I love you, too'){
+    retun ""
+  }
